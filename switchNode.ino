390fb5609e9a90814dc3c328c7d6a050eb8f2b0c
yurysvirida <yury.svirida@gmail.com>
2017-04-14 23:48:40 +0300

    copyrights and comments

--- switchNode.ino
+++ switchNode.ino
@@ -1,9 +1,32 @@
+/**
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+ * network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2015 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+**/
+
+
 // Enable debug prints to serial monitor
 #define MY_DEBUG
 
-#define MY_NODE_ID 0xC9
-//0xF3
-//0xC9
+#define MY_NODE_ID 0xAA
+
+// Type of switches connected to the white JST connectors on the board.
+// Momentary is notebook style key.
+#define MOMENTARY_SWITCH
 
 // Enable and select radio type attached
 #define MY_RADIO_RFM69
@@ -11,37 +34,28 @@
 
 //#define MY_RADIO_NRF24
 
-//#define MY_TRANSPORT_WAIT_READY_MS   (2000ul)
-
-//Enable Crypto Authentication to secure the node
-#define MY_SIGNING_ATSHA204
+// Enable Crypto Authentication to secure the node
+//#define MY_SIGNING_ATSHA204
 //#define  MY_SIGNING_REQUEST_SIGNATURES
 
-//Enable OTA feature
+// Enable OTA feature
 #define MY_OTA_FIRMWARE_FEATURE
 #define MY_OTA_FLASH_JDECID 0x2020
 
-//#define MY_BAUD_RATE 19200
 #include <SPI.h>
 #include <MySensors.h>
 
-//#define SPIFLASH_BLOCKERASE_32K   0x52
 #define SPIFLASH_BLOCKERASE_32K   0xD8
 #define SPIFLASH_CHIPERASE        0x60
-
 
 #define SKETCH_NAME "Switch Node "
 #define SKETCH_MAJOR_VER "2"
 #define SKETCH_MINOR_VER "0"
 
 // Initialising array holding button child ID's
-// NULL values used to indicate no switch attached to the corresponding switch connecctor and no child ID need to be added in a Controller
-int SWITCH_CHILD_ID[4] = {1, NULL, NULL, NULL};
+// NULL values used to indicate no switch attached to the corresponding switch connector and no child ID need to be added in a Controller
+int SWITCH_CHILD_ID[4] = {1, 2, 3, NULL};
 
-
-// Type of switch connected to the white JST connector on the board. A2 arduino Pin
-// Momentary is notebook style key.
-#define MOMENTARY_SWITCH
 
 #define BUTTONS_INTERUPT_PIN 3
 
@@ -56,12 +70,7 @@ MyMessage msg_switch[4];
 
 void before()
 {
-
-  //clock_prescale_set(clock_div_8); // Switch to 1Mhz for the reminder of the sketch, save power.
-  // highfreq = false;
-
-
-  //rfm reset pin
+  // RFM reset pin
   pinMode(9, OUTPUT);
   digitalWrite(9, 0);
   // Setup the buttons
@@ -94,42 +103,25 @@ void setup() {
 // Loop will iterate on changes on the BUTTON_PINs
 void loop()
 {
-  // Button state value arrays
+  // Buttons state values array
   static uint8_t  value[4] = {NULL, NULL, NULL, NULL};
   static  uint8_t last_value[4] = {NULL, NULL, NULL, NULL};
 
-  //------------------------------------------------------- get the battery Voltage
+  // Get the battery Voltage
   int sensorValue = analogRead(BATTERY_SENSE_PIN);
-  /*
-     #ifdef MY_DEBUG
-     Serial.println(sensorValue);
-     #endif
-  */
   // 1M, 470K divider across battery and using internal ADC ref of 1.1V
   // Sense point is bypassed with 0.1 uF cap to reduce noise at that point
   // ((1e6+470e3)/470e3)*1.1 = Vmax = 3.44 Volts
   // 3.44/1023 = Volts per bit = 0.003363075
 
   int batteryPcnt = sensorValue / 10;
-  /*
-     #ifdef MY_DEBUG
-     float batteryV  = sensorValue * 0.00312805;
-     Serial.print("Battery Voltage: ");
-     Serial.print(batteryV);
-     Serial.println(" V");
-
-     Serial.print("Battery percent: ");
-     Serial.print(batteryPcnt);
-     Serial.println(" %");
-     #endif
-  */
   if (oldBatteryPcnt != batteryPcnt) {
     // Power up radio after sleep
     sendBatteryLevel(batteryPcnt);
     oldBatteryPcnt = batteryPcnt;
   }
-  //   Check active switches
 
+// Check active switches
 #ifdef MOMENTARY_SWITCH
   for (int i = 0; i <= 3; i++) {
     if (SWITCH_CHILD_ID[i] != NULL) {
@@ -166,6 +158,5 @@ void loop()
   }
   wait(100);
 #endif
-  //if (_radio.readRSSI() < CSMA_LIMIT){Serial.print(" canSend():"); Serial.println(_radio.readRSSI());}
   sleep(BUTTONS_INTERUPT_PIN - 2, RISING, 0);
 }
